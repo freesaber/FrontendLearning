9d75b29208b5c6392b6109d740eb7b1e85724c66
freesaber <saber.lover@qq.com>
2019-08-22 09:26:54 +0800

    页面刷新404

--- 04-vue/huangyi-elm/vue-sell/src/router.js
+++ 04-vue/huangyi-elm/vue-sell/src/router.js
@@ -7,7 +7,7 @@ import seller from './views/seller.vue'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  // mode: 'history', 该模式会导致刷新404
   base: process.env.BASE_URL,
   routes: [
     {
